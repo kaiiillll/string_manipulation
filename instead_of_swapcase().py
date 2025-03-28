e882d24edce4fb03266cfedbbb4752b69d282cce
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-28 21:54:25 +0800

    instead_of_swapcase()

--- instead_of_swapcase().py
+++ instead_of_swapcase().py
@@ -1,3 +1,13 @@
 # use def function
-# use if-elif-else statement to run the condition
-# return the result after undergoes boolean operator
+def swap_case(text):
+    result = ""
+    for letter in text:
+        # use if-elif-else statement to run the condition
+        if 'A' <= letter <= 'Z':  
+            result += chr(ord(letter) + 32)  # Convert uppercase to lowercase
+        elif 'a' <= letter <= 'z':  
+            result += chr(ord(letter) - 32)  # Convert lowercase to uppercase
+        else:
+            result += letter  
+    return result # return the result after undergoes boolean operator
+
