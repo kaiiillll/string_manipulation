d10baee474ffe5f88c631962dbc9f1bade56bef5
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-28 21:45:30 +0800

    instead_of_center().py

--- instead_of_center().py
+++ instead_of_center().py
@@ -1,4 +1,8 @@
 # use def function
-# if statement 
-# left and right spaces
-# return the text under the condition
+def center_text(text, width):
+    if len(text) < width:  # if statement 
+        spaces = width - len(text)  # Find how many spaces needed
+# Divide spaces on both sides
+        right_spaces = spaces - left_spaces  # Make sure total spaces match
+        text = (" " * left_spaces) + text + (" " * right_spaces)  # Add spaces
+    return text  # return the text under the condition
