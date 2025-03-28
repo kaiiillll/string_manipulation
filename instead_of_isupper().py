9ee759dfabc95ab6ddd2f811f4e0701bbc719600
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-28 21:24:38 +0800

    instead_of_isupper()

--- instead_of_isupper().py
+++ instead_of_isupper().py
@@ -1,3 +1,6 @@
 # use def function 
-# use for loop 
-# use if statement and return if false under condition
+def check_uppercase(text):
+    for letter in text:  #  use for Loop through each character
+        if 'a' <= letter <= 'z':  # Check if any lowercase letter exists
+            return False  #  use If statement found, return False (not all uppercase)
+    return True 
