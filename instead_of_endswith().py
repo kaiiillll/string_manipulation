a4dab31262b4c7081b5e8674dc845db5c824e7e0
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-28 21:35:05 +0800

    instead_of_endswith()

--- instead_of_endswith().py
+++ instead_of_endswith().py
@@ -1,2 +1,8 @@
-# use def function 
-# if statement to check the condition and return if false
+def check_ends_with(text, ending): # use def function 
+    text_length = len(text)
+    ending_length = len(ending)
+
+    if text[text_length - ending_length:] == ending:  # if statement to check the condition and return if false
+
+        return True
+    return False
