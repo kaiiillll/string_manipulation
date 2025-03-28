bb73775229187d4ba9938f403232d92795e07f07
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-03-28 19:51:25 +0800

    instead_of_removeprefix()

--- instead_of_removeprefix().py
+++ instead_of_removeprefix().py
@@ -1,3 +1,5 @@
-# use def dunction 
-# use if statement
-# use the return to remove the prefix
+def remove_prefix(text, prefix): # use def dunction 
+    length = len(prefix)  
+    if text[:length] == prefix:  # # use if statement to Check if text starts with prefix
+        return text[length:]  # use the return to remove the prefix
+    return text
