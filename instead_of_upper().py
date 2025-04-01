f56e7507b730eb190f2d624e20490bcfa7924291
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 12:29:00 +0800

    instead_of_upper()

--- instead_of_upper().py
+++ instead_of_upper().py
@@ -1,4 +1,13 @@
 # use def function to do specfic task
-# use if-else statement for checking under condition
-# return if true
-# get user input
+def to_uppercase(words):
+    result = ""
+    for character in words:
+        if 'a' <= character <= 'z': # use if-else statement for checking under condition
+            result += chr(ord(character) - 32)
+        else:
+            result += character
+    return result  # return if true
+
+# Get user input
+words = input("Enter a string: ")
+print("Uppercase:", to_uppercase(words))
