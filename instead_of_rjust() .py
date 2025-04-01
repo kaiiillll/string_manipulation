219f68f300cdffb99cb24f2102ef2271298267fb
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 16:32:38 +0800

    instead_of_rjust()

--- instead_of_rjust() .py
+++ instead_of_rjust() .py
@@ -1,4 +1,10 @@
 # use def dunction
-# return under the condition
+def right_justify(words, length):
+    return ' ' * (length - len(words)) + words # return under the condition
+
 # get the user input 
+user_input = input("Enter a string: ")
+length = int(input("Enter the total length: "))
+
 # print the results
+print(right_justify(user_input, length))
