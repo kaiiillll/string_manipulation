fc7b37956967e1553b684d31bc81213f3fde0166
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 11:09:27 +0800

    instead_of_rstrip()

--- instead_of_rstrip().py
+++ instead_of_rstrip().py
@@ -1,4 +1,13 @@
 # use def function
-# use while loop for loop checks
-# return with plus one of words
-# 
+def remove_trailing_spaces(s):
+    words = len(s) - 1
+    while words >= 0 and s[words] == ' ': # use while loop for loop checks
+        words -= 1
+    return s[:words+1] # return with plus one of words
+
+# Get user input
+user_input = input("Enter a string with trailing spaces: ")
+trimmed_string = remove_trailing_spaces(user_input)
+
+print(f'Original: "{user_input}"')
+print(f'Trimmed: "{trimmed_string}"')
