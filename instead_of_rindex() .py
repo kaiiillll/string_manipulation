b288e3c179bd1782664bdc1db6f5cce0cd8e5624
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 17:12:25 +0800

    instead_of_rindex()

--- instead_of_rindex() .py
+++ instead_of_rindex() .py
@@ -1,7 +1,12 @@
-# function custom_rindex(string, char)
-# for position FROM length of string - 1 DOWN TO 0
-# if statemnt character at position in string equals char
-# return position
-# end of for loop
-# return if not found 
+# def function
+def custom_rindex(string, char):
+    for position in range(len(string)-1, -1, -1):
+        if string[position] == char:  # if statemnt character at position in string equals char
+            return position # return position
+    return -1
+
+first_input = input("Enter a string: ")
+second_input = input("Enter a character: ")
+
+print(custom_rindex(first_input, second_input))
 # end of function
