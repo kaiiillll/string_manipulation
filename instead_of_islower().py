a302809c277b006f9099c5d164533a08555711ac
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 12:34:58 +0800

    instead_of_islower()

--- instead_of_islower().py
+++ instead_of_islower().py
@@ -1,5 +1,13 @@
 # use def function to do task specifically
-# use for loop
-# use if statement to check under the condition
+def check_all_lowercase(words):
+    for character in words:  # use for loop
+        if character < 'a' or character > 'z':  # use if statement to check under the condition that will check the lower
+            return False
+    return True
+
 # get the user input
+user_input = input("Enter a string: ")
+
 # print the modified inputs
+is_lowercase = check_all_lowercase(user_input)
+print(f'Is the string all lowercase? {is_lowercase}')
