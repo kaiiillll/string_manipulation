f36bec72737cbc0d95446e88202b3b427fbf3013
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 17:02:04 +0800

    instead_of_index()

--- instead_of_index().py
+++ instead_of_index().py
@@ -1,5 +1,12 @@
-
 # for loop each word in the string 
-# if statement the character at index word in string is equal to char
-# return word (index of the character)
+def custom_index(string, char): # use def function
+    for word in range(len(string)):
+        if string[word] == char:  # if statement the character at index word in string is equal to char
+            return word -1
+
+first_input = input("Enter a string: ")
+second_input = input("Enter a character: ")
+
+result = custom_index(first_input, second_input)
+print(result)
 # end the for loop
