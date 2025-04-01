8e3a08e286165fedfa85af21dcb9a8c8b11e8209
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 11:22:47 +0800

    instead_of_removesuffix()

--- instead_of_removesuffix().py
+++ instead_of_removesuffix().py
@@ -1,4 +1,14 @@
+# Get user input
+user_input = input("Enter a string: ")
+suffix_to_remove = input("Enter the suffix to remove: ")
+trimmed_string = remove_suffix(user_input, suffix_to_remove)
+
 # use def function
-# use if statement for checking condition
-# use return 
-# get user input
+def remove_suffix(words, suffix):
+    if words.endswith(suffix): # use if statement for checking condition
+        return wordwords[:len(words) - len(suffix)] # use return 
+    return words
+
+print(f'Original: "{user_input}"')
+print(f'Trimmed: "{trimmed_string}"')
+
