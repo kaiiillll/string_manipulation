14e10551ff833ffbfc6d137aa5f66d6d06ab50b1
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 17:05:56 +0800

    pseudocode

---
+++ instead_of_rindex() .py
@@ -0,0 +1,7 @@
+# function custom_rindex(string, char)
+# for position FROM length of string - 1 DOWN TO 0
+# if statemnt character at position in string equals char
+# return position
+# end of for loop
+# return if not found 
+# end of function
