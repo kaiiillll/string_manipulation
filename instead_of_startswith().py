f8b304f019de5e2f9745333cabc2a1639617076a
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-01 12:45:24 +0800

    instead_of_startswith()

--- instead_of_startswith().py
+++ instead_of_startswith().py
@@ -1,4 +1,12 @@
+# use def function
+def check_starts_with(words, prefix):
+    for words in range(len(prefix)): # use for loop
+        if words[words] != prefix[words]: # under if statement check the condition and return if true
+            return False
+    return True
 # Get user input
-# check the given string 
-# use the for loop 
-# under if statement check the condition and return if true
+user_input = input("Enter a string: ")
+prefix = input("Enter the prefix to check: ")
+
+print(check_starts_with(user_input, prefix))
+
